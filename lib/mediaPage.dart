8088903fe9a4c8da36c073bd684a301dba72435e
Ella <ellal1688@gmail.com>
2022-08-09 12:54:10 -0500

    media page in progress

--- lib/mediaPage.dart
+++ lib/mediaPage.dart
@@ -1,27 +1,163 @@
 import 'package:flutter/material.dart';
-import 'package:tulip_app/mediaButton.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 
 class mediaPage extends StatelessWidget {
-  final mediaButtons = [
-    'assets/BasicsButton.png',
-    'assets/MentalHealthButton.png'
-  ];
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My App",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: MainPage(),
+    );
+  }
+}
 
-  Widget build(BuildContext) {
+class MainPage extends StatefulWidget {
+  const MainPage({Key key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage>
+    with SingleTickerProviderStateMixin {
+  TabController controller;
+  @override
+  void initState() {
+    super.initState();
+    controller = new TabController(vsync: this, length: 4, initialIndex: 0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle.dark.copyWith(statusBarColor: Colors.red[400]));
     return Scaffold(
       appBar: AppBar(
-        title: Text("My Videos"),
-        centerTitle: true,
-        backgroundColor: Color.fromRGBO(247, 178, 178, 1.0),
+        backgroundColor: Colors.red[400],
+        title: Container(
+          margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
+          decoration: BoxDecoration(
+            color: Color.fromARGB(50, 255, 255, 255),
+            borderRadius: BorderRadius.all(Radius.circular(22.0)),
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 5.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Search Video",
+                      hintStyle: TextStyle(color: Colors.white),
+                      icon: Icon(Icons.search, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 0,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 5.0),
+                  child: Row(
+                    children: <Widget>[
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.mic, color: Colors.white),
+                      ),
+                      VerticalDivider(color: Colors.white),
+                      IconButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const HomePage()),
+                          );
+                        },
+                        icon: Icon(Icons.home, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
+    );
+  }
 
-      /*
-        body: Column(
-          children: <Widget>[
-            mediaButton(mediaButtons[0]),
-            mediaButton(mediaButtons[1])
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+        .add(DiagnosticsProperty<TabController>('controller', controller));
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List data = ["One", "Two", "Three", "Four"];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 70,
+            ),
+            SizedBox(
+              height: 200,
+              child: ListView.builder(
+                itemCount: 10,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) => Container(
+                  height: 100,
+                  width: 100,
+                  margin: EdgeInsets.all(10),
+                  child: Center(
+                    child: Text("Card $index"),
+                  ),
+                  color: Colors.pinkAccent[200],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 200,
+            ),
+            SizedBox(
+              height: 200,
+              child: ListView.builder(
+                itemCount: 10,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) => Container(
+                  height: 100,
+                  width: 100,
+                  margin: EdgeInsets.all(10),
+                  child: Center(
+                    child: Text("Card $index"),
+                  ),
+                  color: Colors.pinkAccent[200],
+                ),
+              ),
+            )
           ],
-        )*/
+        ),
+      ),
     );
   }
 }
